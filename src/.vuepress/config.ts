f00385f82e467009f2c01f7a4d27592b0a2b8ec0
hello8693 <1320998105@qq.com>
2024-08-29 15:45:33 +0800

    chore: 更新VuePress配置文件和主题链接

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/classisland-docs-next/",
 
   lang: "zh-CN",
   title: "ClassIsland文档",
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -4,7 +4,7 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "https://docs.classisland.tech",
+  hostname: "https://classisland.github.io",
 
   author: {
     name: "ClassIsland社区",
