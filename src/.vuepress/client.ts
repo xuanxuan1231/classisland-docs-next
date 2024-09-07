c13ae374aeab165c91ef34b21023514ccbb7af07
WRC <hello_wrc@outlook.com>
2024-09-07 10:36:42 +0800

    接入 Sentry 分析

---
+++ src/.vuepress/client.ts
@@ -0,0 +1,24 @@
+import { defineClientConfig } from 'vuepress/client'
+import * as Sentry from "@sentry/vue";
+
+export default defineClientConfig({
+  enhance({ app, router, siteData }) {
+    Sentry.init({
+        app,
+        dsn: "https://f300bd95cb322030809d33bcf5bb8fa2@sentry.classisland.tech/6",
+        integrations: [
+          Sentry.browserTracingIntegration(),
+          Sentry.replayIntegration(),
+        ],
+        // Tracing
+        tracesSampleRate: 1.0, //  Capture 100% of the transactions
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+        // Session Replay
+        replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+      });
+  },
+  setup() {},
+  rootComponents: [],
+})
